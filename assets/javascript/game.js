c18bca5b9518dc450f9cac88656d64582507d2f1
Rey Coronado <rcorojr@gmail.com>
2020-05-02 19:56:18 -0600

    add var for gems + random number. Add variables for wins, losses, and guess total.

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+// random number generator
+
+$(document).ready(function() {
+
+// random number generator
+    var randNum = Math.floor(Math.random() * ((120-19) +1) + 19);
+    $('#randomnumber').html(randNum);
+    
+    var unique = Math.floor(Math.random() * 12) +1;
+    var pink = Math.floor(Math.random() * 12) +1;
+    var green = Math.floor(Math.random() * 12) +1;
+    var red = Math.floor(Math.random() * 12) +1;
+
+
+    var wins = 0;
+    var losses = 0;
+    var guesstot = 0;
+
+})
