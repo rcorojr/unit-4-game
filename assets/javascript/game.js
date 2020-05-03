64efad534efd532fbbd290543132e5dbc45c7042
Rey Coronado <rcorojr@gmail.com>
2020-05-02 21:12:50 -0600

    begin if/else statments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,34 +8,49 @@ $(document).ready(() => {
 
 
 })
+
+//random number generator for buttons
 var unique = Math.floor(Math.random() * 12) +1;
 var pink = Math.floor(Math.random() * 12) +1;
 var green = Math.floor(Math.random() * 12) +1;
 var red = Math.floor(Math.random() * 12) +1;
-
 
 var wins = 0;
 var losses = 0;
 var guesstot = 0;
 
 
+
+//onclick events for buttons to add ot the guess total
 $('#green').click(function() {
 guesstot = guesstot + green
 $('#guesstot').text(guesstot);
+if (guesstot === randNum){
+
+}
 })
 
 $('#pink').click(function() {
 guesstot = guesstot + pink
 $('#guesstot').text(guesstot);
+if (guesstot === randNum)
 })
 
 $('#unique').click(function() {
 guesstot = guesstot + unique
 $('#guesstot').text(guesstot);
+if (guesstot === randNum) {
+
+}
 })
 
 $('#red').click(function() {
 guesstot = guesstot + red
 $('#guesstot').text(guesstot);
-$
+if (guesstot === randNum){
+
+}
+
 })
+
+//finish if/else statements to recognize a win and a loss. wins and losses need to increase by one
