1c87c2e0ee5770bf07998a5efb2d4de1f70d5f4f
Rey Coronado <rcorojr@gmail.com>
2020-05-02 21:03:57 -0600

    add click events. update buttons.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,4 +16,26 @@ var red = Math.floor(Math.random() * 12) +1;
 
 var wins = 0;
 var losses = 0;
-var guesstot = 0; 
+var guesstot = 0;
+
+
+$('#green').click(function() {
+guesstot = guesstot + green
+$('#guesstot').text(guesstot);
+})
+
+$('#pink').click(function() {
+guesstot = guesstot + pink
+$('#guesstot').text(guesstot);
+})
+
+$('#unique').click(function() {
+guesstot = guesstot + unique
+$('#guesstot').text(guesstot);
+})
+
+$('#red').click(function() {
+guesstot = guesstot + red
+$('#guesstot').text(guesstot);
+$
+})
